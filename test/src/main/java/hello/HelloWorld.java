7cad117dff9ea491fd1ce2e3a7b33c67d8eaa7d9
hello_ buaa <hello_ buaa@hellobuaa>
2016-11-12 15:10:33 +0800

    feat

---
+++ test/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,28 @@
+package hello;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet{
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+	             throws ServletException, IOException{
+		  System.out.println("service method");
+		  super.service(req, resp);
+	}
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException
+    {
+        System.out.println("deGet method");
+        PrintWriter pw = resp.getWriter();
+
+        pw.println("Hello World");
+        pw.close();
+    }
+}
